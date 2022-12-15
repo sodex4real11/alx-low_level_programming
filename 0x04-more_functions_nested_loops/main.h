359c999a1cad6b3665be8c155acb8845a9521203
sodex4real11 <sodex4real11@gmail.com>
2022-12-15 03:47:20 -0800

    More functions

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * file -main.h
+ * Author: sodex4real11
+ * Desc: Header contains protptypes for all functions used in the task
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif
