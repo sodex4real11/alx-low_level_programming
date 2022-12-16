cf53348717257acb40b71eaaa82ffd14ca7d36e6
sodex4real11 <sodex4real11@gmail.com>
2022-12-16 14:15:57 -0800

    more functions

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,13 +9,15 @@ int main(void)
 {
 	long int n, fp;
 
-	n = 612852475143
+	n = 612852475143;
 	for (fp = 2; fp <= n; fp++)
 	{
 		if (n % fp == 0)
 		{
-			n /= fp; fp--
+			n /= fp;
+			fp--;
 		}
+
 	}
 	printf("%ld\n", fp);
 	return (0);
