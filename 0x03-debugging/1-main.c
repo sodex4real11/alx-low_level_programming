98616addab2df5a5aadfae86730a6d9a0dfaea2d
sodex4real11 <sodex4real11@gmail.com>
2022-12-16 09:57:30 -0800

    debugging task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,16 +9,17 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 	/**
-	* while (i < 10)
-	* {
-	*	putchar(i);
-	* }
-	*/
-	printf("infinite loop avoided! \\o/\n");
+	 * while (i < 10)
+	 * {
+	 *	putchar(i);
+	 * }
+	 *
+	 */
+	printf("Infinite loop avoided! \\0/\n");
 
 	return (0);
 }
