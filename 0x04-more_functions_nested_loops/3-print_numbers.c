ca76838e987a8899574a726780be18c43593905b
sodex4real11 <sodex4real11@gmail.com>
2022-12-16 01:37:38 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print all numbers from 0 to 9
+ *
+ * Return: returns void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n + '0');
+	_putchar ('\n');
+	}
+}
