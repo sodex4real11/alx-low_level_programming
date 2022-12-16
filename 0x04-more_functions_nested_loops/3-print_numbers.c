6c9c7a3cd0ea9d868412e1c697da2c5565cd9a48
sodex4real11 <sodex4real11@gmail.com>
2022-12-16 02:12:29 -0800

    more functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,11 @@
 
 void print_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c = 0; c <= 9; c++)
+	for (n = 0; n <= 9; n++)
 	{
-	_putchar(c);
+	_putchar((n % 10) + '0');
 	}
 	_putchar ('\n');
-	
 }
