04de2274bfb92e46e8d5ae2da3e80e055a304b86
sodex4real11 <sodex4real11@gmail.com>
2022-12-16 11:48:12 -0800

    more functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,21 +2,35 @@
 
 /**
  * print_number - prints an integer
- * @n- The integer to be printed
+ * @n- input integer
+ * Return: no return
  */
 
 void print_number(int n)
 {
-	unsigned int d = n;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-	_putchar('_');
-	d = -d;
+		_putchar(45);
+		m = n * -1;
 	}
-	if ((d / 10) > 0)
+	else
 	{
-	print_number(d / 10);
+		m = n;
 	}
-	_putchar((d % 10) + '0')
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
