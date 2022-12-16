15d1ce6caa7fb7a58c1b6cf4cf1183af45991f33
sodex4real11 <sodex4real11@gmail.com>
2022-12-16 10:52:22 -0800

    more functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,11 @@
 
 void print_line(int n)
 {
-	int line;
+	int i;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-	for (line = 0; line <= n; line++)
-	{
-	_putchar('_');
-	}
+		_putchar(95);
 	}
 	_putchar('\n');
 }
