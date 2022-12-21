d05a04677226133d96956b5c121d71d23d41c334
sodex4real11 <sodex4real11@gmail.com>
2022-12-20 22:26:56 -0800

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
  */
 void rev_string(char *s)
 {
-	"sodiq"->"qidos"
+	"sam"->"mas"
 	char tmp;
 	int i, length1, length2;
 
@@ -17,8 +17,8 @@ void rev_string(char *s)
 	while (s[length1] != '\0')
 		length1++;
 
-	length2 = length1 - 1
-	for (i = 0; i < length1; i++)
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
 	{
 		tmp = s[i];
 		s[i] = s[length2];
