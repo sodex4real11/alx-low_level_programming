b516189e98a1e6751cd2410ba3c7052dbac47279
sodex4real11 <sodex4real11@gmail.com>
2022-12-20 20:30:44 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to be printed in reverse
+ *
+ * Return: nothing on success
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		s++;
+		count++;
+	}
+
+	while (count)
+	{
+		s--;
+		_putchar(*s);
+		count--;
+	}
+
+	_putchar('\n');
+}
