c2b64f28b1dbd837fa3116f13b1e3b7dfb6bdb43
sodex4real11 <sodex4real11@gmail.com>
2022-12-20 21:14:11 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * point_array - prints n element of an array of integers, followed by \
+ * a new line
+ * @a: array to be printed
+ * @n: number of element in array
+ *
+ * Return: nothing on success
+ */
+
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		printf("%d", a[count]);
+
+		if (count != (n - 1))
+			printf(", ");
+		count++;
+	}
+	putchar('\n')
+}
