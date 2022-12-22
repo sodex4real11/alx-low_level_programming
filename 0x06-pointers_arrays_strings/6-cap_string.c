66b4d26e361d333bce832e8e1b0398916965a319
sodex4real11 <sodex4real11@gmail.com>
2022-12-22 04:58:51 -0800

    more pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,10 @@
 char *cap_string(char *s)
 {
 	int count 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59,
-46, 33, 63, 34, 40, 41, 123, 125};
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + count) >= 97 && *(s +
-count) <= 122)
-		*(s + count) = *(s +
-count) -32;
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
 	count++;
 	while (*(s + count) != '\0')
 	{
@@ -22,10 +19,8 @@ count) -32;
 		{
 			if (*(s + count) == sep_words[i])
 			{
-				if ((*(s + (count + 1)) >= 97)
-				&& (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) =
-			*(s + (count + 1)) - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
 		}
