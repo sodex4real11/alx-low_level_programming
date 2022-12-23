3d5a66ba26550f6d9dbc578a48d408d2912c0bd8
sodex4real11 <sodex4real11@gmail.com>
2022-12-22 17:44:08 -0800

    more pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * leet - leet function
- * @s: param s
+ * @x: param x
  * Return: a string
  */
 
-char *leet(char *s)
+char *leet(char *x)
 {
 	int a = 0, b, l = 5;
 	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
