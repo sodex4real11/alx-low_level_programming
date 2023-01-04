f1e1a54b3c4c2c2c8b1342b61fd53cb523050461
sodex4real11 <sodex4real11@gmail.com>
2023-01-04 08:03:25 -0800

    recursions

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * _print_rev_recursion - print a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: string
- * Return: No return
+ *
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
