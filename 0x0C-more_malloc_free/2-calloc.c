d797a63198d58c722a3f846283af1b318bb2bd11
sodex4real11 <sodex4real11@gmail.com>
2023-01-13 10:47:44 -0800

    more malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,13 +16,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size);
+	void p = malloc(nmemb * size);
+
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < (nmemb * size) i++)
+	for (i = 0; i < (nmemb * size); i++)
+
 	{
 		*((char *)(p) + i) = 0;
 	}
