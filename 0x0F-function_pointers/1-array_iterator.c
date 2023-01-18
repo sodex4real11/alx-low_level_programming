60835f62c881227bf7e04a3ebbdea605e0899c9d
sodex4real11 <sodex4real11@gmail.com>
2023-01-18 03:17:17 -0800

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,13 +7,18 @@
  * @array: array to iterate over
  * @size: size of the array
  * @action: pointer to function used
+ *
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i;
 
-	if (!array || !action)
-		return;
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
 }
