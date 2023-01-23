8daf1802500fb661302a320afc1e408b36ab3ae4
sodex4real11 <sodex4real11@gmail.com>
2023-01-23 12:21:45 -0800

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,7 +11,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	unsigned int i;
 	va_list valist;
-	
+
 	if (n == 0)
 		return (0);
 
@@ -19,20 +19,10 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	va_start(valist, n);
-<<<<<<< HEAD
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
 
 	va_end(valist);
-
-=======
-	
-	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
-	
-	va_end(valist);
-	
->>>>>>> c705d5d24a8bcd401f84a49497eca2a912bdde81
 	return (sum);
 }
