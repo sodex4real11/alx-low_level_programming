472608ccfe16d2f02b334e194b5af4247f0322e5
sodex4real11 <sodex4real11@gmail.com>
2023-01-29 05:29:52 -0800

    new commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -35,14 +35,14 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-		new->str = strdup(str);
+	new->str = strdup(str);
 	if (new->str == NULL)
 	{
 		free(new);
-			return (NULL);
+		return (NULL);
 	}
 	new->len = _strlen(new->str);
-		new->next = *head;
-		*head = new;
+	new->next = *head;
+	*head = new;
 	return (new);
 }
