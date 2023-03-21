2539aef4ca320c5c16914db383397f1fe968f260
sodex4real11 <sodex4real11@gmail.com>
2023-03-20 21:06:32 -0700

    task new

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string given
+ * @c: character to search
+ * Return: pointer to the first occurrence of the character c in the string s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0') /*declaring WHILE*/
+	{
+		if (*s == c) /*if s == s*/
+		{
+			return (s); /*return s*/
+		}
+
+		++s;
+	}
+
+	if (*s == c)
+	{
+		return (s);
+
+	}
+
+	return (0);/* values null*/
+}
